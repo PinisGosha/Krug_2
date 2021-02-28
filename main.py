f271df29b75cee4eb32911ab877844e169705641
Пинус Георгий <74608515+PinisGosha@users.noreply.github.com>
2021-02-28 18:48:03 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import random
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5 import uic
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('круг')
+        self.pbt = QPushButton('Круг', self)
+        self.pbt.move(150, 150)
+        self.pbt.clicked.connect(self.push_flag)
+        self.flag = False
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_elips(qp)
+            qp.end()
+
+    def draw_elips(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        hw = random.randint(50, 300)
+        x = random.randint(0, 400)
+        y = random.randint(0, 400)
+        qp.drawEllipse(x, y, hw, hw)
+
+
+    def push_flag(self):
+        self.flag = True
+        self.repaint()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+© 2021 GitHub, Inc.
